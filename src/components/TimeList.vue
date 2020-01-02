95630c1301e7a272e3ea0fdf801c218d14c7728f
YHevIs <2392022530@qq.com>
2020-01-02 15:10:33 +0800

    基础构建_2

--- src/components/TimeList.vue
+++ src/components/TimeList.vue
@@ -1,27 +1,190 @@
 <template>
-  <div class="time-list">
-      <h2>Time list</h2>
-      <ul>
-        <li>
-          <div>
-            <div></div>
-          </div>
-        </li>
-      </ul>
+  <div class="time-list" id="time-list">
+    <h2>Time list</h2>
+    <div id="timelineContainer" class="timelineContainer">
+      <div class="timelineMajor">
+        <h2 class="timelineMajorMarker">2015</h2>
+        <dl class="timelineMinor">
+          <dt id="event01">
+            <a>事件标题</a>
+          </dt>
+          <dd class="timelineEvent" id="event01EX" style="display: none; ">
+            <p>事件内容.</p>
+          </dd>
+          <!-- /.timelineEvent -->
+        </dl>
+        <!-- /.timelineMinor -->
+        <dl class="timelineMinor">
+          <dt id="event02">
+            <a>又一事件</a>
+          </dt>
+          <dd class="timelineEvent" id="event02EX" style="display: none; ">
+            <p>内容</p>
+          </dd>
+          <!-- /.timelineEvent -->
+        </dl>
+        <!-- /.timelineMinor -->
+      </div>
+      <div class="timelineMajor">
+        <h2 class="timelineMajorMarker">2016</h2>
+        <dl class="timelineMinor">
+          <dt id="event03">
+            <a>事件标题</a>
+          </dt>
+          <dd class="timelineEvent" id="event03EX" style="display: none; ">
+            <p>事件内容.</p>
+          </dd>
+          <!-- /.timelineEvent -->
+        </dl>
+        <!-- /.timelineMinor -->
+        <dl class="timelineMinor">
+          <dt id="event4">
+            <a>又一事件</a>
+          </dt>
+          <dd class="timelineEvent" id="event04EX" style="display: none; ">
+            <p>内容</p>
+          </dd>
+          <!-- /.timelineEvent -->
+        </dl>
+        <!-- /.timelineMinor -->
+      </div>
+      <div class="timelineMajor">
+        <h2 class="timelineMajorMarker">2017</h2>
+        <dl class="timelineMinor">
+          <dt id="event05">
+            <a>事件标题</a>
+          </dt>
+          <dd class="timelineEvent" id="event05EX" style="display: none; ">
+            <p>事件内容.</p>
+          </dd>
+          <!-- /.timelineEvent -->
+        </dl>
+        <!-- /.timelineMinor -->
+        <dl class="timelineMinor">
+          <dt id="event6">
+            <a>又一事件</a>
+          </dt>
+          <dd class="timelineEvent" id="event06EX" style="display: none; ">
+            <p>内容</p>
+          </dd>
+          <!-- /.timelineEvent -->
+        </dl>
+        <!-- /.timelineMinor -->
+      </div>
+      <div class="timelineMajor">
+        <h2 class="timelineMajorMarker">2018</h2>
+        <dl class="timelineMinor">
+          <dt id="event07">
+            <a>事件标题</a>
+          </dt>
+          <dd class="timelineEvent" id="event07EX" style="display: none; ">
+            <p>事件内容.</p>
+          </dd>
+          <!-- /.timelineEvent -->
+        </dl>
+        <!-- /.timelineMinor -->
+        <dl class="timelineMinor">
+          <dt id="event8">
+            <a>又一事件</a>
+          </dt>
+          <dd class="timelineEvent" id="event08EX" style="display: none; ">
+            <p>内容</p>
+          </dd>
+          <!-- /.timelineEvent -->
+        </dl>
+        <!-- /.timelineMinor -->
+      </div>
+      <div class="timelineMajor">
+        <h2 class="timelineMajorMarker">2019</h2>
+        <dl class="timelineMinor">
+          <dt id="event09">
+            <a>事件标题</a>
+          </dt>
+          <dd class="timelineEvent" id="event09EX" style="display: none; ">
+            <p>事件内容.</p>
+          </dd>
+          <!-- /.timelineEvent -->
+        </dl>
+        <!-- /.timelineMinor -->
+        <dl class="timelineMinor">
+          <dt id="event10">
+            <a>又一事件</a>
+          </dt>
+          <dd class="timelineEvent" id="event10EX" style="display: none; ">
+            <p>内容</p>
+          </dd>
+          <!-- /.timelineEvent -->
+        </dl>
+        <!-- /.timelineMinor -->
+      </div>
+      <div class="timelineMajor">
+        <h2 class="timelineMajorMarker">2020</h2>
+        <dl class="timelineMinor">
+          <dt id="event11">
+            <a>事件标题</a>
+          </dt>
+          <dd class="timelineEvent" id="event11EX" style="display: none; ">
+            <p>事件内容.</p>
+          </dd>
+          <!-- /.timelineEvent -->
+        </dl>
+        <!-- /.timelineMinor -->
+        <dl class="timelineMinor">
+          <dt id="event12">
+            <a>又一事件</a>
+          </dt>
+          <dd class="timelineEvent" id="event12EX" style="display: none; ">
+            <p>内容</p>
+          </dd>
+          <!-- /.timelineEvent -->
+        </dl>
+        <!-- /.timelineMinor -->
+      </div>
+      <!-- /.timelineMajor -->
+    </div>
+    <!-- /#timelineContainer -->
   </div>
 </template>
 
 <script>
+import anime from "animejs";
 export default {
-  name: 'time-list',
-}
+  name: "time-list",
+  mounted() {
+    // window.console.log(anime)
+    anime({
+      targets: ".css-prop-demo",
+      backgroundColor: "#FFF",
+      borderRadius: ["0%", "50%"],
+      easing: "easeInOutQuad",
+      // width:'1000',
+      duration: 1000
+    });
+  }
+};
 </script>
 
 
 <style>
-.time-list{
+.time-list {
   width: 60%;
-  border-right: 2px solid rgba(200,200,200,.5);
+  border-right: 2px solid rgba(200, 200, 200, 0.5);
   padding: 20px;
+}
+.timeListButton {
+  background: #f9f9f9;
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  box-shadow: 0 2px 2px 2px rgba(0, 0, 0, 0.35);
+}
+.timelistButtonBackground {
+  background: #f9f9f9;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
