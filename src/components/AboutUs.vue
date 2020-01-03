b2da01c92bda1c2a889c3a50ec0ca1f397e55850
YHevIs <2392022530@qq.com>
2020-01-03 10:21:44 +0800

    基础构建_2

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="about-us">
-      <h2>About us</h2>
-      <p>
-           Decoration15(1) is a united, enterprising class full of ambition and competitive spirit. In daily life, we are positive, unassertive, and individual and stick to our dreams. Our class is made up of 49 small units. We believe that with a dedicated teacher and diligent students, our class would be a happy, humorous family. We believe that Decoration15(1) would be better in the future.
-      </p>
+      <h2>About me</h2>
+      <p>BEYONDSOFT Web Developer to Date.</p>
+      <p><b>BLOG:</b>&nbsp;<a href="https://yhevis.top">www.yhevis.top</a></p>
+      <p><b>Project address:</b><a href="https://github.com/YHevIs/Class-official-website">github.com/YHevIs/Class-official-website</a></p>
   </div>
 </template>
 
@@ -18,5 +18,8 @@ export default {
   .about-us{
     padding: 20px 0 20px 8%;
     flex:1;
+  }
+  .about-us p{
+    margin: 10px 0;
   }
 </style>
---
+++ src/components/MessageBoard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="message-board">
+    <h2>Message Board</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'message-board',
+}
+</script>
+
+<style>
+  .message-board{
+    padding: 10px 20px;
+  }
+</style>
